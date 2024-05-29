ba57672ed407f4e65b939add8a17acaef489e74e
EvanMworia <evannji99@gmail.com>
2024-05-29 14:45:57 +0300

    Setup Config files

---
+++ src/app.ts
@@ -0,0 +1,4 @@
+const usersBASEURL = 'http://localhost:3000/users/';
+const productsBASEURL = 'http://localhost:3000/products/';
+const cartBASEURL = 'http://localhost:3000/cart/';
+const ordersBASEURL = 'http://localhost:3000/orders/';
